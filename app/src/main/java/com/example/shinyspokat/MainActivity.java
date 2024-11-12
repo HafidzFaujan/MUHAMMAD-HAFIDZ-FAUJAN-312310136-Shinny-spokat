8f2c7fb09d06fdde0dc32c402565edc56145b7e6
HafidzFaujan <hafidzhafidz1933@gmail.com>
2024-11-12 11:54:33 +0700

    finished

---
+++ app/src/main/java/com/example/shinyspokat/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.shinyspokat;
+
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.method.MovementMethod;
+import android.view.View;
+import android.widget.Button;
+import org.w3c.dom.Text;
+
+import java.security.PublicKey;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    public Button listharga;
+    public Button hubungikami;
+    public Button lokasi;
+    public Button sosmed;
+    public Button tentangkami;
+    public Button btnwa;
+
+
+
+
+
+
+        @Override
+        protected void onCreate (Bundle savedInstanceState){
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            lokasi = findViewById(R.id.lokasi);
+            lokasi.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, lokasi.class);
+                    startActivity(intent);
+                }
+            });
+
+            hubungikami = findViewById(R.id.hubungikami);
+            hubungikami.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, hubungikami.class);
+                    startActivity(intent);
+                }
+            });
+
+
+            listharga = findViewById(R.id.list);
+            listharga.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, listharga.class);
+                    startActivity(intent);
+                }
+            });
+
+            sosmed = findViewById(R.id.sosialmedia);
+            sosmed.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, sosmed.class);
+                    startActivity(intent);
+                }
+            });
+
+            tentangkami = findViewById(R.id.tentangkami);
+            tentangkami.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(MainActivity.this, tentangkami.class);
+                    startActivity(intent);
+                }
+            });
+        }
+    }
+
+
+
+
+
---
+++ app/src/main/java/com/example/shinyspokat/listharga.java
@@ -0,0 +1,13 @@
+package com.example.shinyspokat;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class listharga extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listharga);
+    }
+}
